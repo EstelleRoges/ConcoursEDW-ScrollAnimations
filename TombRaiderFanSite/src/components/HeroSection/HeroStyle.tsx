984be556e8cc4a6a4c8d8024a0948c8a20554126
EstelleR <73401176+EstelleRoges@users.noreply.github.com>
2023-09-22 16:30:02 +0200

    Video not found error fix attempt

--- TombRaiderFanSite/src/components/HeroSection/HeroStyle.tsx
+++ TombRaiderFanSite/src/components/HeroSection/HeroStyle.tsx
@@ -3,14 +3,13 @@ import styled from "styled-components";
 export const HeroContainer = styled.section`
 width: 100%;
 height: 100vh;
-
+position: relative;
 overflow: hidden;
 
 `
 
 export const HeroVideo = styled.video`
-
-
+position: absolute;
 width: 100%;
 height: 100%;
 object-fit: cover;
