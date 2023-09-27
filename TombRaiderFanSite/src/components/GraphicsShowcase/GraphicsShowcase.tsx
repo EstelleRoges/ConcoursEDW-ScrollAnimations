4e0db204c4eae2a36e004687add30139ac68dfd6
EstelleR <73401176+EstelleRoges@users.noreply.github.com>
2023-09-27 17:30:31 +0200

    Update
    Graphics showcase animation partially working; need to generalize the code

--- TombRaiderFanSite/src/components/GraphicsShowcase/GraphicsShowcase.tsx
+++ TombRaiderFanSite/src/components/GraphicsShowcase/GraphicsShowcase.tsx
@@ -1,3 +1,4 @@
+import { RefObject, useState, useEffect, useRef, SetStateAction } from "react";
 import { GraphicsComparison } from "../ComponentsIndex";
 import * as GraphicsShowcaseStyle from "./GraphicsShowcaseStyle";
 
@@ -6,22 +7,89 @@ import screenshot_tr2 from "../../assets/images/Tr2Image.jpg";
 import screenshot_tr3 from "../../assets/images/Tr3Image.jpg";
 
 const GraphicsShowcase = () => {
+  const tr1Container = useRef<HTMLDivElement | null>(null);
+  const tr2Container = useRef<HTMLDivElement | null>(null);
+  const tr3Container = useRef<HTMLDivElement | null>(null);
+
+  const [isTr1ContainerDisplayed, setIsTr1ContainerDisplayed] = useState(false);
+  const [isTr2ContainerDisplayed, setIsTr2ContainerDisplayed] = useState(false);
+  const [isTr3ContainerDisplayed, setIsTr3ContainerDisplayed] = useState(false);
+  const [isTrContainerDisplayed, setIsTrContainerDisplayed] = useState(false);
+
+  console.log(isTrContainerDisplayed);
+  const observeAndDisplay = (
+    containerToObserve: RefObject<HTMLDivElement | null>,
+    setState: React.Dispatch<React.SetStateAction<S>>
+  ) => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (containerToObserve.current && entries[0].isIntersecting) {
+          const elementToDisplay = entries[0].target as HTMLDivElement;
+          setState(true);
+          elementToDisplay.style.animation = "imageGradient";
+          observer.unobserve(elementToDisplay);
+        }
+      },
+      {
+        root: null,
+        threshold: 0.6,
+        rootMargin: "0px",
+      }
+    );
+
+    containerToObserve && observer.observe(containerToObserve.current);
+    setState(false);
+
+    return () => {
+      if (containerToObserve) {
+        observer.unobserve(containerToObserve.current);
+      }
+    };
+  };
+
+  useEffect(() => {
+    observeAndDisplay(tr1Container, setIsTrContainerDisplayed);
+    observeAndDisplay(tr2Container, setIsTrContainerDisplayed);
+    observeAndDisplay(tr3Container, setIsTrContainerDisplayed);
+  }, []);
+
   return (
     <div id="graphicsShowcase">
       <GraphicsComparison />
       <GraphicsShowcaseStyle.ShowcaseSection>
         <GraphicsShowcaseStyle.ShowcaseContainer>
-          <GraphicsShowcaseStyle.SingleGameCatchphrase>
-            <p>Partez à la recherche du Scion, artefact puissant au savoir millénaire</p>
-            <img src={screenshot_tr1} alt="newGraphicsScreenshots_1" />
+          <GraphicsShowcaseStyle.SingleGameCatchphrase
+            ref={tr1Container}
+            $isTrContainerDisplayed={isTrContainerDisplayed}
+          >
+            <p>
+              Partez à la recherche du Scion, artefact puissant au savoir
+              millénaire
+            </p>
+            <GraphicsShowcaseStyle.TrImage>
+              <GraphicsShowcaseStyle.Gradient
+                $isTrContainerDisplayed={isTrContainerDisplayed}
+              />
+              <img src={screenshot_tr1} alt="newGraphicsScreenshots_1" />
+            </GraphicsShowcaseStyle.TrImage>
           </GraphicsShowcaseStyle.SingleGameCatchphrase>
-          <GraphicsShowcaseStyle.SingleGameCatchphrase>
-            <p>Retrouvez la la dague de Xian avant qu'elle ne tombe entre de mauvaise mains</p>
-            <img src={screenshot_tr2} alt="newGraphicsScreenshots_2" />
+          <GraphicsShowcaseStyle.SingleGameCatchphrase ref={tr2Container}>
+            <p>
+              Retrouvez la la dague de Xian avant qu'elle ne tombe entre de
+              mauvaise mains
+            </p>
+            <GraphicsShowcaseStyle.TrImage>
+              <img src={screenshot_tr2} alt="newGraphicsScreenshots_2" />
+            </GraphicsShowcaseStyle.TrImage>
           </GraphicsShowcaseStyle.SingleGameCatchphrase>
-          <GraphicsShowcaseStyle.SingleGameCatchphrase>
-            <p>Parcourez le monde à la recherche de mystérieux artefacts et perçez leurs secrets</p>
-            <img src={screenshot_tr3} alt="newGraphicsScreenshots_3" />
+          <GraphicsShowcaseStyle.SingleGameCatchphrase ref={tr3Container}>
+            <p>
+              Parcourez le monde à la recherche de mystérieux artefacts et
+              perçez leurs secrets
+            </p>
+            <GraphicsShowcaseStyle.TrImage>
+              <img src={screenshot_tr3} alt="newGraphicsScreenshots_3" />
+            </GraphicsShowcaseStyle.TrImage>
           </GraphicsShowcaseStyle.SingleGameCatchphrase>
         </GraphicsShowcaseStyle.ShowcaseContainer>
       </GraphicsShowcaseStyle.ShowcaseSection>
--- TombRaiderFanSite/src/components/GraphicsShowcase/GraphicsShowcaseStyle.tsx
+++ TombRaiderFanSite/src/components/GraphicsShowcase/GraphicsShowcaseStyle.tsx
@@ -1,27 +1,21 @@
 import styled from "styled-components";
 
+interface SingleGameProps {
+  $isTr1ContainerDisplayed?: boolean;
+  $isTr2ContainerDisplayed?: boolean;
+  $isTr3ContainerDisplayed?: boolean;
+  $isTrContainerDisplayed?: boolean;
+}
+
 export const ShowcaseSection = styled.section``;
 export const ShowcaseContainer = styled.article``;
 
-export const SingleGameCatchphrase = styled.div`
+export const SingleGameCatchphrase = styled.div<SingleGameProps>`
   height: 100vh;
   position: relative;
-
-  &::after {
-    content: "";
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-image: linear-gradient(
-      to right,
-      transparent 30%,
-      var(--backgroundColor) 100%
-    );
-    background-size: 200% 100%;
-    animation: imageGradient 1s;
-  }
+  opacity: ${({ $isTrContainerDisplayed }) =>
+    $isTrContainerDisplayed ? 1 : 0};
+  transition: opacity 0.3s ease-out;
 
   p {
     margin: 5rem 3rem 3rem;
@@ -31,6 +25,7 @@ export const SingleGameCatchphrase = styled.div`
     font-size: 2rem;
     font-style: italic;
     text-shadow: 0 0 0.3rem var(--backgroundColor);
+    z-index: 2;
   }
 
   img {
@@ -38,4 +33,38 @@ export const SingleGameCatchphrase = styled.div`
     height: auto;
     display: block;
   }
+`;
+
+export const TrImage = styled.div`
+  position: relative;
+  z-index: 1;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    box-shadow: inset 0 0 0.5rem 1rem var(--backgroundColor);
+  }
+`;
+
+export const Gradient = styled.div<SingleGameProps>`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 3;
+
+  background-image: linear-gradient(
+    to right,
+    var(--backgroundColor) 50%,
+    transparent 51%,
+    transparent 99%,
+    var(--backgroundColor) 100%
+  );
+    background-size: 200% 100%;
+    animation: ${({ $isTrContainerDisplayed }) =>
+      $isTrContainerDisplayed ? "imageGradient 1s forwards" : "none"};
 `;
